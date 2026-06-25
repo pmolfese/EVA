1d5279eda742ce0ec9066bd5236089aae19c21a7
Peter Molfese <pmolfese@gmail.com>
2026-06-25 09:30:12 -0400

    Initial Commit

---
+++ EVA/EVAApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVAApp.swift
+//  EVA
+//
+//  Created by Molfese, Peter  [E] on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct EVAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EVATests/EVATests.swift
@@ -0,0 +1,19 @@
+//
+//  EVATests.swift
+//  EVATests
+//
+//  Created by Molfese, Peter  [E] on 6/25/26.
+//
+
+import Testing
+@testable import EVA
+
+struct EVATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
