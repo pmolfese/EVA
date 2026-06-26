bd1e8f38ae1f4a0fe349a6f4d0c6e91ed76db52e
Peter Molfese <pmolfese@gmail.com>
2026-06-26 11:18:32 -0400

    copyright stuff

--- Tools/convert_iclabel_to_coreml.py
+++ Tools/convert_iclabel_to_coreml.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+# Developed by P. Molfese, National Institutes of Health (NIH).
+#
+# This software is a "work of the United States Government" prepared by a federal
+# employee as part of official duties. As such, it is not subject to copyright
+# protection within the United States (17 U.S.C. § 105). International copyrights
+# may apply.
+#
+# Released under the terms of the GNU General Public License, version 3 (GPL-3.0).
+# The U.S. Government authorizes the distribution and modification of this software
+# subject to the copyleft requirements of the GPL-3.0.
 # SPDX-License-Identifier: GPL-3.0-only
-# Copyright (C) 2026 Peter Molfese
 """Convert the official ICLabel MatConvNet weights to Core ML.
 
 Usage:
